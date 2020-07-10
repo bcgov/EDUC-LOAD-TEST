cc1f2d23f151c8222238a8bba1177e884666ce3b
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2020-07-10 10:27:26 -0700

    added mandatory env variables.

--- app/config/index.js
+++ app/config/index.js
@@ -5,11 +5,14 @@ const env = process.env.NODE_ENV;
 
 nconf.argv()
   .env()
-  .file({ file: path.join(__dirname, `${env}.json`) });
+  .file({file: path.join(__dirname, `${env}.json`)});
 nconf.defaults({
   environment: env,
   studentURL: process.env.STUDENT_API_URL,
-  studentGenerator:{
+  tokenUrl: process.env.TOKEN_URL,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  studentGenerator: {
     maxRecords: process.env.maxRecords, // must be a number
     concurrency: process.env.concurrency // must be a number
   }
