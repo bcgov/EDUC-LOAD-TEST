789a122406d340732fb70b722121d07afb35e226
OMPRAKASH MISHRA <32200996+mishraomp@users.noreply.github.com>
2021-08-03 14:30:20 -0700

    Update start.sh

--- k6/openshift/k6/start.sh
+++ k6/openshift/k6/start.sh
@@ -1,2 +1,2 @@
-k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-match-api-test.ts
+#k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-match-api-test.ts
 k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/student-api-test.ts
