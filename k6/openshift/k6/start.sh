5b208c2021d7733543d94b1dcd4790c20f90518f
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-08-06 10:57:32 -0700

    test match api

--- k6/openshift/k6/start.sh
+++ k6/openshift/k6/start.sh
@@ -1,3 +1,3 @@
-#k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-match-api-test.ts
+k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-match-api-test.ts
 #k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/student-api-test.ts
-k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/soam-api-test.ts
+#k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/soam-api-test.ts
