7aaff87da4314b50333514e7400495b8aff1a1b5
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-08-25 14:56:05 -0700

    to run test in TEST

--- k6/openshift/k6/start.sh
+++ k6/openshift/k6/start.sh
@@ -1,6 +1,6 @@
 #k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-match-api-test.ts
 #k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/student-api-test.ts
-#k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/soam-api-test.ts
+k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/soam-api-test.ts
 #k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-request-api-test.ts
 #k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/student-profile-api-test.ts
-k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-services-api-test.ts
+#k6 run -e CONFIG=/var/opt/config/config.json /var/opt/scripts/pen-services-api-test.ts
